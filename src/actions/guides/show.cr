f3ebb0578d13ecc44e08d113d02d23e136e8351a
Paul Smith <paulcsmith0218@gmail.com>
2019-04-27 17:19:06 -0400

    Raise if linking to path that doesn't exist

--- src/actions/guides/show.cr
+++ src/actions/guides/show.cr
@@ -14,6 +14,19 @@ class Guides::Show < BrowserAction
   end
 
   def guide_path
+    self.class.guide_path(guide_filename)
+  end
+
+  def self.guide_path(guide_filename)
     "./src/guides/#{guide_filename}.md"
+  end
+
+  def self.route(guide_filename)
+    guide_path = guide_path(guide_filename)
+    if File.exists?(guide_path)
+      previous_def(guide_filename)
+    else
+      raise "Couldn't find a guide at: #{guide_path}"
+    end
   end
 end
