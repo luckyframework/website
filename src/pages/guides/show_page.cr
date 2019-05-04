07bc1b34f4a8f446043c009b60cc641209f3702d
Paul Smith <paulcsmith0218@gmail.com>
2019-05-04 11:06:46 -0400

    Starting on custom renderer

--- src/pages/guides/show_page.cr
+++ src/pages/guides/show_page.cr
@@ -1,6 +1,14 @@
 class Guides::ShowPage < GuideLayout
   def content
-    options = Markd::Options.new(smart: true)
-    raw Markd.to_html(@markdown, options)
+    raw rendered_markdown
   end
+
+  def rendered_markdown
+    options = Markd::Options.new(smart: true)
+    document = Markd::Parser.parse(@markdown, options)
+    CustomRenderer.new(options).render(document)
+  end
+end
+
+class CustomRenderer < Markd::HTMLRenderer
 end
