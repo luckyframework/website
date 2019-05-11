0308fc5a036191a46307805626c268a6b5b5f524
Paul Smith <paulcsmith0218@gmail.com>
2019-05-10 21:04:37 -0400

    Make indent more noticeable

--- src/components/guides/sidebar.cr
+++ src/components/guides/sidebar.cr
@@ -71,11 +71,11 @@ class Guides::Sidebar < BaseComponent
         categories.each do |category|
           li do
             if category.active?(@current_guide)
-              div class: "block pb-3 bg-grey-lighter border-t border-b border-grey-light shadow-inner" do
+              div class: "block pb-3 bg-grey-lighter border-t border-b border-grey shadow-inner" do
                 span category.title, class: "pl-8 py-3 mb-2 block bold text-sm tracking-wide"
                 category.guides.each do |guide|
                   if guide == @current_guide
-                    link guide.title, guide, class: "block text-sm text-grey-darker no-underline pl-12 py-3 hover:bg-grey-lighter hover:underline #{active_class}"
+                    link guide.title, guide, class: "block text-sm text-grey-darker no-underline pl-12 py-3 hover:underline #{active_class}"
                   else
                     link guide.title, guide, class: "block text-sm text-grey-darker no-underline pl-12 py-3 hover:underline hover:text-blue-dark"
                   end
@@ -94,6 +94,6 @@ class Guides::Sidebar < BaseComponent
   end
 
   def active_class
-    "text-white bg-teal text-shadow hover:bg-lucky-teal-blue hover:no-underline hover:text-white"
+    "text-white bg-teal text-shadow hover:no-underline hover:text-white"
   end
 end
