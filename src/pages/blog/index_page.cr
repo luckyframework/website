b90fe5dab3baa36c7e89e0e892c59e29b26b365e
Paul Smith <paulcsmith0218@gmail.com>
2019-05-15 12:31:19 -0700

    Better styled blog middle section

--- src/pages/blog/index_page.cr
+++ src/pages/blog/index_page.cr
@@ -2,8 +2,8 @@ class Blog::IndexPage < BlogLayout
   needs posts : Array(BasePost)
 
   def middle_section
-    div class: "#{responsive_container_classes} pt-6 pb-8 md:pt-12 md:pb-16" do
-      h1 "Want to stay up to date?", class: " text-white font-normal text-xl md:text-2xl mb-3 text-shadow"
+    div class: "#{responsive_container_classes} pt-8 pb-10 md:pt-12 md:pb-16" do
+      h1 "Want to stay up to date?", class: " text-white font-normal md:text-2xl mb-5 md:mb-3 text-shadow"
       div class: "flex flex-row w-full shadow-lg rounded-lg" do
         input type: "email",
           class: "bg-white p-4 md:p-4 md:text-lg w-full rounded-l md:rounded-l-lg border-b border-teal-darker",
