c52640f9402dc41c01af87fb68ebf5a940b6cd6a
Paul Smith <paulcsmith0218@gmail.com>
2019-05-15 21:32:28 -0700

    Redirect old paths
    Closes #5

---
+++ src/actions/guides/redirect.cr
@@ -0,0 +1,31 @@
+# This action redirects the old guides to the new ones
+class Guides::Redirect < BrowserAction
+  REDIRECTS = {
+    "overview"                   => Guides::GettingStarted::Concepts,
+    "installing"                 => Guides::GettingStarted::Installing,
+    "configuration"              => Guides::GettingStarted::Configuration,
+    "database-migrations"        => Guides::Database::ManagingAndMigrating,
+    "actions-and-routing"        => Guides::HttpAndRouting::RoutingAndParams,
+    "rendering-html"             => Guides::Frontend::RenderingHtml,
+    "querying-the-database"      => Guides::Database::Querying,
+    "custom-queries"             => Guides::Database::RawSql,
+    "saving-with-forms"          => Guides::Database::ValidatingSavingDeleting,
+    "asset-handling"             => Guides::Frontend::AssetHandling,
+    "tasks"                      => Guides::CommandLineTasks::BuiltIn,
+    "logging-and-error-handling" => Guides::Logging,
+    "writing-json-apis"          => Guides::JsonAndApis::RenderingJson,
+    "generating-test-data"       => Guides::Database::Testing,
+    "browser-tests"              => Guides::Frontend::Testing,
+    "sending-emails"             => Guides::Emails::SendingEmailsWithCarbon,
+    "authentication"             => Guides::Authentication::Show,
+    "deploying-heroku"           => Guides::Deploying::Heroku,
+  }
+
+  get "/guides/:guide_path" do
+    if new_guide = REDIRECTS[guide_path]?
+      redirect to: new_guide, status: HTTP::Status::MOVED_PERMANENTLY
+    else
+      raise Lucky::RouteNotFoundError.new(context)
+    end
+  end
+end
