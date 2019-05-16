ef7f9b6290be72667f3433a0964427ef0bc559d9
Paul Smith <paulcsmith0218@gmail.com>
2019-05-16 00:48:31 -0700

    Hook up subscriber form
    Closes #17

--- src/pages/blog/index_page.cr
+++ src/pages/blog/index_page.cr
@@ -4,13 +4,20 @@ class Blog::IndexPage < BlogLayout
   def middle_section
     div class: "#{responsive_container_classes} pt-8 pb-10 md:pt-12 md:pb-16" do
       h1 "Want to stay up to date?", class: " text-white font-normal text-2xl mb-5 md:mb-3 text-shadow"
-      div class: "flex flex-row w-full shadow-lg rounded-lg" do
-        input type: "email",
-          class: "bg-white p-4 md:p-4 md:text-lg w-full rounded-l md:rounded-l-lg border-b border-teal-darker",
-          placeholder: "Enter your email"
-        input type: "submit",
-          value: "Subscribe",
-          class: "py-3 px-5 rounded-r md:rounded-r-lg cursor-pointer text-white text-shadow uppercase tracking-wide font-bold bg-lucky-green-vertical-gradient hover:bg-lucky-green-vertical-gradient-dark text-sm border-b border-teal-darker"
+
+      form action: "https://gmail.us20.list-manage.com/subscribe/post?u=b742935e1ac2e7aabaa8b9da9&id=a7ae9d7f32", class: "validate", id: "mc-embedded-subscribe-form", method: "post", name: "mc-embedded-subscribe-form", novalidate: "", target: "_blank" do
+        div class: "flex flex-row w-full shadow-lg rounded-lg" do
+          input type: "email",
+            name: "EMAIL",
+            class: "bg-white p-4 md:p-4 md:text-lg w-full rounded-l md:rounded-l-lg border-b border-teal-darker",
+            placeholder: "Enter your email"
+          div aria_hidden: "true", style: "position: absolute; left: -5000px;" do
+            input name: "b_b742935e1ac2e7aabaa8b9da9_a7ae9d7f32", tabindex: "-1", type: "text", value: ""
+          end
+          input type: "submit",
+            value: "Subscribe",
+            class: "py-3 px-5 rounded-r md:rounded-r-lg cursor-pointer text-white text-shadow uppercase tracking-wide font-bold bg-lucky-green-vertical-gradient hover:bg-lucky-green-vertical-gradient-dark text-sm border-b border-teal-darker"
+        end
       end
     end
   end
