e4b281f216e08e2a683c8f92d9f67f9d630c471e
Jeremy Woertink <jeremywoertink@gmail.com>
2019-05-21 19:15:33 -0700

    Updating http and routing docs (#32)
    * Updating http and routing docs
    
    * missed an example in response. Rewording some confusing param stuff
    
    * added text and examples on handling request and response headers

--- src/actions/guides/http_and_routing/request_and_response.cr
+++ src/actions/guides/http_and_routing/request_and_response.cr
@@ -7,6 +7,93 @@ class Guides::HttpAndRouting::RequestAndResponse < GuideAction
 
   def markdown
     <<-MD
+    ## Handling Requests
+
+    When a request comes in to an Action, Lucky gives you access to the request object through the `request` method.
+    > The `request` object is an instance of [HTTP::Request](https://crystal-lang.org/api/HTTP/Request.html).
+
+    Lucky gives you access to a few helpful methods based on the request Content-Type.
+
+    * `json?` - true if the Content-Type header is "application/json"
+    * `ajax?` - true if the X-Requested-With header is "XMLHttpRequest"
+    * `html?` - true if the Content-Type header is "text/html"
+    * `xml?` - true if the Content-Type header is "application/xml" or "application/xhtml+xml"
+    * `plain?` - true if the Content-Type header is "text/plain"
+
+    You can use these methods to help direct the request or return different responses.
+
+    ```crystal
+    class Users::Show < BrowserAction
+      route do
+        if json?
+          # The Content-Type is a json request, so let's return some json
+          json(Users::ShowSerializer.new(current_user))
+        else
+          # Just render the page like normal
+          render Users::ShowPage
+        end
+      end
+    end
+    ```
+
+    ### Accessing Headers
+
+    If you need to access the request headers, you can use the `headers` method.
+
+    ```crystal
+    class Dashboard::Index < BrowserAction
+      route do
+        remote_ip = headers["X-Forwarded-For"]?
+        if remote_ip
+          text "The remote IP is \#{remote_ip}"
+        else
+          text "No remote IP found"
+        end
+      end
+    end
+    ```
+
+    ## Handling Responses
+
+    Once you've recieved your request, and handled it, you'll need to return a response. Every Lucky::Action requires that a response is returned.
+
+    * `render` - render a Lucky::HTMLPage
+    * `redirect` - redirect the request to another location
+    * `text` - respond with plain text
+    * `json` - return a json response
+    * `head` - return a head response with a 204 status
+    * `file` - return a file for download
+
+    ```crystal
+    class Jobs::Reports::Create < ApiAction
+      post "/jobs/reports/" do
+        # Run some fancy background job
+        if plain?
+          # plain text request, return some plain text
+          text "Job sent for processing"
+        else
+          # Respond with HEAD 201
+          head 201
+        end
+      end
+    end
+    ```
+
+    > The `response` object is an instance of [HTTP::Server::Response](https://crystal-lang.org/api/HTTP/Server/Response.html)
+
+    ### Setting Response Headers
+
+    For things like handling CORS, and many other operations like cacheing, it may be necessary to set special response headers. Set these values through the `response.headers` object.
+
+    ```crystal
+    class Admin::Reports::Show < BrowserAction
+      route do
+        response.headers["Cache-Control"] = "max-age=150"
+        render ShowPage
+      end
+    end
+    ```
+
     ## Redirecting
 
     You can redirect using the `redirect` method:
@@ -25,6 +112,14 @@ class Guides::HttpAndRouting::RequestAndResponse < GuideAction
       end
     end
     ```
+
+    ### Redirect statuses
+
+    The default status for a redirect is `Lucky::Action::Status::Found` (302), but if you need a different status code, you can pass any [Lucky Action Status Enum](https://github.com/luckyframework/lucky/blob/v0.14.0/src/lucky/action.cr#L20)
+
+
+
+
     MD
   end
 end
--- src/actions/guides/http_and_routing/sessions_and_cookies.cr
+++ src/actions/guides/http_and_routing/sessions_and_cookies.cr
@@ -37,20 +37,52 @@ class Guides::HttpAndRouting::SessionsAndCookies < GuideAction
 
     ### Clearing Sessions and Cookies
 
-    If you need to remove a session
+    If you need to remove a session, or delete a cookie.
 
     ```crystal
     # Delete a specific session key
     session.delete(:name)
+    session.deleted?(:name) #=> true
 
     # Clear the current session
     session.clear
 
     # Delete a specific cookie
     cookies.delete("name")
+    cookies.deleted?("person") #=> false
 
     # Delete all cookies
     cookies.clear
+    ```
+
+    ### Customize Cookies
+
+    If you need to customize specific [cookie options](https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie), each cookie is an instance of [HTTP::Cookie](https://crystal-lang.org/api/HTTP/Cookie.html) which gives you access to several helpful methods.
+
+    ```crystal
+    # This gives you a HTTP::Cookie or raise an exception
+    c_is_for_cookie = cookies.get_raw("cookiedough")
+
+    # Set the name of the cookie to "cookie-dough"
+    c_is_for_cookie.name("cookie-dough")
+
+    # Set the value of cookie-dough to "yum"
+    c_is_for_cookie.value("yum")
+
+    # Set the path for the cookie to "/baking"
+    c_is_for_cookie.path("/baking")
+
+    # Set the expires date to tomorrow
+    c_is_for_cookie.expires(1.day.from_now)
+
+    # Set the domain to cookie.monst.er
+    c_is_for_cookie.domain("cookie.monst.er")
+
+    # Make the cookie secure
+    c_is_for_cookie.secure(true)
+
+    # Set the cookie to HTTP only
+    c_is_for_cookie.http_only(true)
     ```
     MD
   end
