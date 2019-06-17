fc13ea393b53a74dc33bff8816e60ec0cc818b3a
Jeremy Woertink <jeremywoertink@gmail.com>
2019-06-17 08:55:30 -0700

    formatted to 80(ish) character for easier reading. Missed 2 links

--- src/actions/guides/database/database_setup.cr
+++ src/actions/guides/database/database_setup.cr
@@ -37,23 +37,30 @@ class Guides::Database::DatabaseSetup < GuideAction
     * retry_attempts
     * retry_delay
 
-    To set the connection pool options, just append a query string to the end of the `settings.url` option (e.g. `?initial_pool_size=5&retry_attempts=2`)
+    To set the connection pool options, just append a query string to the end of the
+    `settings.url` option (e.g. `?initial_pool_size=5&retry_attempts=2`)
 
     ### Avram Configuration
 
-    Avram requires a `url` option to be set. If you decide to not use Avram as your ORM, you can set this option to a string like `"unused"`.
+    Avram requires a `url` option to be set. If you decide to not use Avram as your ORM,
+    you can set this option to a string like `"unused"`.
 
-    Optionally, the `lazy_load_enabled` is set to `false` for development and test. This causes Lucky to raise an exception if you forget to preload an association, but will not raise an exception in production.
+    Optionally, the `lazy_load_enabled` is set to `false` for development and test.
+    This causes Lucky to raise an exception if you forget to preload an association,
+    but will not raise an exception in production.
 
     ## Test Setup
 
-    If you'd like to use separate credentials for your testing database, you can add another conditional in `config/database.cr` that checks for `Lucky::Env.test?` and sets the `setting.url` option to the appropriate value.
+    If you'd like to use separate credentials for your testing database, you can add
+    another conditional in `config/database.cr` that checks for `Lucky::Env.test?` and
+    sets the `setting.url` option to the appropriate value.
 
     ## Create and Drop database
 
     ### Create database
 
-    To create your database, run the `lucky db.create` task. This will create the database named from `database_name` in your `config/database.cr` file.
+    To create your database, run the `lucky db.create` task. This will create the database
+    named from `database_name` in your `config/database.cr` file.
 
     ### Drop database
 
@@ -63,13 +70,16 @@ class Guides::Database::DatabaseSetup < GuideAction
 
     ## Seeding Data
 
-    Seeding is the process of putting data in to your database. This could be fake placeholder data you use in development, or even special data your application expects to exist in production.
+    Seeding is the process of putting data in to your database. This could be fake placeholder
+    data you use in development, or even special data your application expects to exist in production.
 
-    By default, Lucky generates two tasks in your app's `tasks/` folder. `Db::CreateRequiredSeeds`, and `Db::CreateSampleSeeds`. You can use [Boxes]() or [Forms]() to create the data.
+    By default, Lucky generates two tasks in your app's `tasks/` folder. `Db::CreateRequiredSeeds`,
+    and `Db::CreateSampleSeeds`. You can use [Boxes](#{Guides::Database::Testing.path}) or [Forms](#{Guides::Database::ValidatingSavingDeleting.path}) to create the data.
 
     ### Required Seeds
 
-    Let's say you're getting ready to launch your application to production for the very first time. You may need an initial Admin user account that will be able to login and create your other Admin accounts.
+    Let's say you're getting ready to launch your application to production for the very first time.
+    You may need an initial Admin user account that will be able to login and create your other Admin accounts.
 
     This code will go in `tasks/create_required_seeds.cr`.
 
@@ -90,7 +100,8 @@ class Guides::Database::DatabaseSetup < GuideAction
 
     ### Sample Seeds
 
-    This data is a great way to fill your development database with fake placeholder data to mimic a fully functioning production database without the worry of losing production data.
+    This data is a great way to fill your development database with fake placeholder data
+    to mimic a fully functioning production database without the worry of losing production data.
 
     This code will go in `tasks/create_sample_seeds.cr`.
 
@@ -110,7 +121,9 @@ class Guides::Database::DatabaseSetup < GuideAction
 
     Run this task with `lucky db.create_sample_seeds`.
 
-    > Running `./script/setup` in development will run the `db.create_sample_seeds` task for you. If you need to re-seed, you can run `lucky db.drop` and then `./script/setup` to re-create and seed your local database.
+    > Running `./script/setup` in development will run the `db.create_sample_seeds` task for you.
+    > If you need to re-seed, you can run `lucky db.drop` and then `./script/setup` to re-create
+    > and seed your local database.
     MD
   end
 end
