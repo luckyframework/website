4be1832551d2dbc33cc86d104f21335cc07e0004
Jeremy Woertink <jeremywoertink@gmail.com>
2019-06-25 09:55:19 -0700

    adding in open graph tags. fixes #61

--- src/components/shared/layout_head.cr
+++ src/components/shared/layout_head.cr
@@ -13,10 +13,22 @@ class Shared::LayoutHead < BaseComponent
 
       responsive_meta_tag
       meta name: "mobile-web-app-capable", content: "yes"
+      open_graph_tags
 
       script src: "https://buttons.github.io/buttons.js", attrs: [:async, :defer]
       css_link "https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css"
-      script type: "text/javascript", src: "https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js"
+      script src: "https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js"
     end
+  end
+
+  # These control how cards are displayed when pages are shared
+  # on social media. http://ogp.me/
+  private def open_graph_tags
+    meta property: "og:title", content: @seo.page_title
+    meta property: "og:url", content: Home::Index.url
+    meta property: "og:type", content: "website"
+    meta property: "og:description", content: @seo.page_description
+    meta property: "og:locale", content: "en_US"
+    meta property: "og:image", content: Lucky::RouteHelper.settings.base_uri + asset("images/lucky_og_screenshot.png")
   end
 end
