5136bffa1f2fa700da207aab3288ee5da6ee8cf9
Robert Guss <robert.guss@insuremytrip.com>
2019-08-12 19:08:26 -0400

    added responsive styles to footer

--- src/components/shared/footer.cr
+++ src/components/shared/footer.cr
@@ -2,12 +2,12 @@ class Shared::Footer < BaseComponent
   def render
     footer class: "py-4" do
       div class: "container mx-auto" do
-        div class: "flex flex-wrap md:flex-no-wrap justify-between items-center md:px-6" do
-          div do
+        div class: "flex flex-wrap justify-center md:justify-between items-center md:px-6" do
+          div class: "flex flex-col items-center md:flex-row" do
             footer_link("Code of Conduct", "https://github.com/luckyframework/lucky/blob/master/CODE_OF_CONDUCT.md")
             footer_link("Contributors", "https://github.com/luckyframework/lucky/graphs/contributors")
           end
-          div class: "flex flex-wrap md:flex-no-wrap justify-between items-center md:px-6" do
+          div class: "flex flex-wrap md:flex-no-wrap justify-between items-center py-4 md:py-0 md:px-6" do
             footer_icon("https://github.com/luckyframework/lucky", "icons/github.svg", "Github")
             footer_icon("https://twitter.com/luckyframework", "icons/twitter.svg", "Twitter")
             footer_icon("https://gitter.im/luckyframework/Lobby", "icons/gitter.svg", "Gitter")
@@ -18,7 +18,7 @@ class Shared::Footer < BaseComponent
   end
 
   private def footer_link(title, url)
-    a title, class: "inline-block text-grey-darkest no-underline mr-2 mb-2 px-2 py-2 hover:text-black ", href: url
+    a title, class: "inline-block text-grey-darkest no-underline md:mr-2 md:mb-2 px-2 py-2 hover:text-black ", href: url
   end
 
   private def footer_icon(url, icon_path, alt_desc)
