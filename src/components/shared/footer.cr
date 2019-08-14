612603c7d40a3452690f1743c233c95398f219b9
Robert Guss <robert.guss@insuremytrip.com>
2019-08-14 13:14:38 -0400

    changed footer_icon() from a macro to a method in footer.cr

--- src/components/shared/footer.cr
+++ src/components/shared/footer.cr
@@ -8,9 +8,9 @@ class Shared::Footer < BaseComponent
             footer_link("Contributors", "https://github.com/luckyframework/lucky/graphs/contributors")
           end
           div class: "flex flex-wrap md:flex-no-wrap justify-between items-center py-4 md:py-0 md:px-6" do
-            footer_icon("https://github.com/luckyframework/lucky", "icons/github.svg", "Github")
-            footer_icon("https://twitter.com/luckyframework", "icons/twitter.svg", "Twitter")
-            footer_icon("https://gitter.im/luckyframework/Lobby", "icons/gitter.svg", "Gitter")
+            footer_icon("https://github.com/luckyframework/lucky", asset("icons/github.svg"), "Github")
+            footer_icon("https://twitter.com/luckyframework", asset("icons/twitter.svg"), "Twitter")
+            footer_icon("https://gitter.im/luckyframework/Lobby", asset("icons/gitter.svg"), "Gitter")
           end
         end
       end
@@ -21,9 +21,9 @@ class Shared::Footer < BaseComponent
     a title, class: "inline-block text-grey-darkest no-underline md:mr-2 md:mb-2 px-2 py-2 hover:text-black ", href: url
   end
 
-  macro footer_icon(url, icon_path, alt_desc)
-    link({{ url }}, class: "ml-6 md:ml-0 mr-10 self-center") do
-      img src: asset({{ icon_path }}), class: "h-8", alt: {{ alt_desc }}
+  private def footer_icon(url, icon_path, alt_desc)
+    link(url, class: "ml-6 md:ml-0 mr-10 self-center") do
+      img src: icon_path, class: "h-8", alt: alt_desc
     end
   end
 end
