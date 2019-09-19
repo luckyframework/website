946a41890d3b0d6bc72a66601000b9f568169a82
Paul Smith <paulcsmith@users.noreply.github.com>
2019-09-19 16:27:05 -0400

    Use correct class name

--- src/actions/guides/http_and_routing/link_generation.cr
+++ src/actions/guides/http_and_routing/link_generation.cr
@@ -13,7 +13,7 @@ class Guides::HttpAndRouting::LinkGeneration < GuideAction
 
     You can access them as class methods on the action itself. They are:
 
-    * `route` - will return a `LuckyWeb::RouteHelper` object that contains both the
+    * `route` - will return a `Lucky::RouteHelper` object that contains both the
       path, the HTTP method, and the full URL as methods `path`, `method`, `url`
     * `with` - an alias for `route` that is used for passing parameters
     * `path` - will return a string of just the path
