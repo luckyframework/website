b275cbd47f32add20d5414b05df4b019d6761d48
Wesley Moore <wes@wezm.net>
2019-10-09 07:28:33 +1100

    Make RSS feed generation a bit less generic

--- src/actions/blog/feed.cr
+++ src/actions/blog/feed.cr
@@ -1,31 +1,6 @@
 class Blog::Feed < BrowserAction
   get "/blog/feed.xml" do
     posts = PostQuery.new.all.first(10)
-    xml render_feed(posts)
-  end
-
-  private def render_feed(posts)
-    items = posts.map do |post|
-      url = Blog::Show.with(post.slug).url
-      RSS::Item.new(
-        guid: RSS::Guid.new(value: url, is_permalink: true),
-        title: post.title,
-        link: url,
-        description: post.html_content,
-        pub_date: post.published_at,
-      )
-    end
-    last_build_date = posts.map(&.published_at).max
-
-    feed = RSS::Channel.new(
-      title: "Lucky Blog",
-      description: "Lucky is a web framework written in Crystal",
-      link: Blog::Index.url,
-      feed_url: Blog::Feed.url,
-      items: items,
-      last_build_date: last_build_date,
-    )
-
-    feed.to_xml
+    xml RSS::Channel.new(posts).to_xml
   end
 end
--- src/utils/rss.cr
+++ src/utils/rss.cr
@@ -2,23 +2,45 @@ require "xml"
 
 module RSS
   class Channel
-    def initialize(@title : String, @link : String, @feed_url : String, @description : String, @items : Array(Item), @last_build_date : Time)
+    def initialize(@posts : Array(BasePost))
     end
 
     def to_xml
       XML.build(encoding: "UTF-8") do |xml|
-        xml.element("rss", "xmlns:content": "http://purl.org/rss/1.0/modules/content/", "xmlns:atom": "http://www.w3.org/2005/Atom", version: "2.0") do
+        xml.element("rss", "xmlns:atom": "http://www.w3.org/2005/Atom", version: "2.0") do
           xml.element("channel") do
-            xml.element("title") { xml.text @title }
-            xml.element("description") { xml.cdata @description }
-            xml.element("link") { xml.text @link }
-            xml.element("atom", "link", nil, rel: "self", href: @feed_url)
-            xml.element("lastBuildDate") { xml.text @last_build_date.to_rfc2822 }
+            xml.element("title") { xml.text "Lucky Blog" }
+            xml.element("description") { xml.text "Lucky is a web framework written in Crystal" }
+            xml.element("link") { xml.text Blog::Index.url }
+            xml.element("atom", "link", nil, rel: "self", href: Blog::Feed.url)
+            xml.element("lastBuildDate") { xml.text last_build_date.to_rfc2822 }
 
-            @items.each &.to_xml(xml)
+            items.each &.to_xml(xml)
           end
         end
       end
+    end
+
+    private def items
+      @posts.map do |post|
+        url = post_url(post)
+
+        RSS::Item.new(
+          guid: RSS::Guid.new(value: url, is_permalink: true),
+          title: post.title,
+          link: url,
+          description: post.html_content,
+          pub_date: post.published_at,
+        )
+      end
+    end
+
+    private def last_build_date
+      @posts.map(&.published_at).max
+    end
+
+    private def post_url(post)
+      Blog::Show.with(post.slug).url
     end
   end
 
