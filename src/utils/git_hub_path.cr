c73e2ea967c81b36820706fe5c333265f5f7bdcb
Jeremy Woertink <jeremywoertink@gmail.com>
2020-03-05 15:22:30 -0800

    checking the edit github link. Attempting to fix #29

--- src/utils/git_hub_path.cr
+++ src/utils/git_hub_path.cr
@@ -2,6 +2,6 @@ module GitHubPath
   extend self
 
   def for_file(file_path = __FILE__)
-    "https://github.com/paulcsmith/website-v2/blob/master#{file_path.gsub(Dir.current, "")}"
+    "https://github.com/luckyframework/website/blob/master#{file_path.gsub(Dir.current, "")}"
   end
 end
