faf222026bf3bb30e745d8edf2bef9dc8464f3d0
Les De Ridder <les@lesderid.net>
2020-03-08 16:06:07 +0100

    Remove references to `.all`
    See https://gitter.im/luckyframework/Lobby?at=5e650773a2ccef3232dd9ec0

--- src/actions/guides/database/querying-deleting.cr
+++ src/actions/guides/database/querying-deleting.cr
@@ -58,13 +58,11 @@ class Guides::Database::QueryingDeleting < GuideAction
     * `find(id)` - Returns the record with the primary key `id`. Raise `Avram::RecordNotFoundError` if no record is found.
     * `last` - Returns the last record. Raise `Avram::RecordNotFoundError` if no record is found.
     * `last?` - Returns the last record. Returns `nil` if no record is found.
-    * `all` - Returns an array of all the records.
 
     ```crystal
     first_user = UserQuery.first
     last_user = UserQuery.last
     specific_user = UserQuery.find(4)
-    all_users = UserQuery.all
     ```
 
     ### Lazy loading
@@ -104,7 +102,7 @@ class Guides::Database::QueryingDeleting < GuideAction
     `SELECT COLUMNS FROM users`
 
     ```crystal
-    users = UserQuery.new.all
+    users = UserQuery.new
     ```
 
     ### Select first
