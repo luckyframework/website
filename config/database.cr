b8e657bf6e0abaa122c5b2476757e151f0d969c7
Paul Smith <paulcsmith@users.noreply.github.com>
2020-04-25 12:55:16 -0400

    Create Lucky CI GitHub action

--- config/database.cr
+++ config/database.cr
@@ -1,16 +1,12 @@
 database_name = "website_v2_#{Lucky::Env.name}"
 
 AppDatabase.configure do |settings|
-  if Lucky::Env.production?
-    settings.url = ENV.fetch("DATABASE_URL")
-  else
-    settings.url = ENV["DATABASE_URL"]? || Avram::PostgresURL.build(
-      database: database_name,
-      hostname: ENV["DB_HOST"]? || "localhost",
-      username: ENV["DB_USERNAME"]? || "postgres",
-      password: ENV["DB_PASSWORD"]? || "postgres"
-    )
-  end
+  settings.url = ENV["DATABASE_URL"]? || Avram::PostgresURL.build(
+    database: database_name,
+    hostname: ENV["DB_HOST"]? || "localhost",
+    username: ENV["DB_USERNAME"]? || "postgres",
+    password: ENV["DB_PASSWORD"]? || "postgres"
+  )
 end
 
 Avram.configure do |settings|
