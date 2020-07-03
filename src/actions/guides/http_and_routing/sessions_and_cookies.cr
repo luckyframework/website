d6c65f2a670c2ae1db7aba66dc618d668ba4fee2
Jeremy Woertink <jeremywoertink@gmail.com>
2020-07-03 10:42:50 -0700

    Adding some guides on disabling cookies. Also updated cookie configuration guides to show more of how to customize. Fixes #326

--- src/actions/guides/http_and_routing/sessions_and_cookies.cr
+++ src/actions/guides/http_and_routing/sessions_and_cookies.cr
@@ -54,7 +54,7 @@ class Guides::HttpAndRouting::SessionsAndCookies < GuideAction
     cookies.clear
     ```
 
-    ### Customize Cookies
+    ## Configuring Cookies
 
     If you need to customize specific [cookie options](https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie), each cookie is an instance of [HTTP::Cookie](https://crystal-lang.org/api/HTTP/Cookie.html), which gives you access to several helpful methods.
 
@@ -82,7 +82,49 @@ class Guides::HttpAndRouting::SessionsAndCookies < GuideAction
 
     # Set the cookie to HTTP only
     c_is_for_cookie.http_only(true)
+
+    # Set the cookie SameSite to Lax. 
+    # You can also use `:strict` here.
+    c_is_for_cookie.samesite(:lax)
     ```
+
+    ### Setting cookie defaults
+
+    The `Lucky::CookieJar` configuration is located in your app's `config/cookies.cr` file.
+    You can set defaults for your cookies when they're written.
+
+    ```crystal
+    # config/cookies.cr
+    Lucky::CookieJar.configure do |settings|
+      settings.on_set = ->(cookie : HTTP::Cookie) {
+        cookie
+          .secure(true)
+          .http_only(true)
+          .samesite(:strict)
+          .expires(1.year.from_now)
+          .domain("mydomain.com")
+      }
+    end
+    ```
+
+    ## Disabling Cookies
+
+    There may be an action where you need disable writing cookies. For this, you
+    can use the `disable_cookies` macro in the action you want to skip writing cookies.
+
+    ```crystal
+    class Api::Posts::Index < ApiAction
+      disable_cookies
+
+      get "/posts" do
+        json([] of Post)
+      end
+    end
+    ```
+
+    This will disable writing all cookies, session, and flash messages for this action.
+    If you need to do this for all of your API, you can add the `disable_cookies` macro to
+    your `ApiAction`. Then all actions that inherit from ApiAction will skip writing cookies.
     MD
   end
 end
