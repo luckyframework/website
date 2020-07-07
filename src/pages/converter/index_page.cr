5a6321df51918dfafef6a16ad9e8a2e76c80cd12
Jeremy Woertink <jeremywoertink@gmail.com>
2020-07-07 08:08:48 -0700

    a little space between the IO blocks

--- src/pages/converter/index_page.cr
+++ src/pages/converter/index_page.cr
@@ -7,7 +7,7 @@ class ConvertHtmlToLucky::IndexPage < PageLayout
       h2 @title, class: "text-3xl mb-4"
       para "Lucky uses Crystal classes and methods to generate HTML. It may sound crazy at first, but the advantages are numerous. Never accidentally print nil to the page, extract and share partials using regular methods. Easily read an entire page by looking at just the render method. Text is automatically escaped for security. And it’s all type safe. That means no more unmatched closing tags, and never rendering a page with missing data."
 
-      div class: "flex my-4" do
+      div class: "flex my-4 space-x-4 items-stretch" do
         div class: "w-1/2" do
           h3 "HTML Input", class: "text-2xl"
           form_for ConvertHtmlToLucky::Create, class: "input-form" do
