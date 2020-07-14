d613d2c642a0ac489ff7161ef6baee57ce3c4f0e
Paul Smith <paulcsmith@users.noreply.github.com>
2020-07-13 20:25:57 -0400

    Change to shorter /html URL (#366)
    And make the namespace a REST resource

--- src/actions/converter/index.cr
+++
@@ -1,8 +0,0 @@
-class ConvertHtmlToLucky::Index < BrowserAction
-  get "/convert-html-to-lucky" do
-    html IndexPage,
-      title: "Convert HTML to Lucky",
-      input: "",
-      output: ""
-  end
-end
