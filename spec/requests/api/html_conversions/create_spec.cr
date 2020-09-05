faeb113f9b9d86273aef485411a2e12e69351454
Jeremy Woertink <jeremywoertink@gmail.com>
2020-09-05 15:55:15 -0700

    Adding a small pause before this API spec gives time for the spec server to be running.

--- spec/requests/api/html_conversions/create_spec.cr
+++ spec/requests/api/html_conversions/create_spec.cr
@@ -2,6 +2,9 @@ require "../../../spec_helper"
 
 describe Api::HtmlConversions::Create do
   it "returns a 200 response with converted content" do
+    # NOTE: This is required because this spec runs
+    # before the spec server is fully booted.
+    sleep 0.5
     response = ApiClient.exec(Api::HtmlConversions::Create.with(input: "<div>Test</div>"))
 
     response.should send_json(200, content: "div \"Test\"\n")
