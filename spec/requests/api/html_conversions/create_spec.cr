3bd8f3304b9ed726ac2c750998b664a04936ae72
Jeremy Woertink <jeremywoertink@gmail.com>
2020-09-05 14:10:35 -0700

    Updated app to Lucky 0.24

--- spec/requests/api/html_conversions/create_spec.cr
+++ spec/requests/api/html_conversions/create_spec.cr
@@ -2,7 +2,7 @@ require "../../../spec_helper"
 
 describe Api::HtmlConversions::Create do
   it "returns a 200 response with converted content" do
-    response = AppClient.exec(Api::HtmlConversions::Create.with(input: "<div>Test</div>"))
+    response = ApiClient.exec(Api::HtmlConversions::Create.with(input: "<div>Test</div>"))
 
     response.should send_json(200, content: "div \"Test\"\n")
   end
@@ -10,7 +10,7 @@ describe Api::HtmlConversions::Create do
   it "returns a 503 (Service Unavailable) when HTML conversions are disabled" do
     ENV["DISABLE_HTML_CONVERSION_API"] = "Shut it down!"
 
-    response = AppClient.exec(Api::HtmlConversions::Create.with(input: "<div>Test</div>"))
+    response = ApiClient.exec(Api::HtmlConversions::Create.with(input: "<div>Test</div>"))
 
     response.status_code.should eq(503)
   end
