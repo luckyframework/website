d102525d5162aa3805c87edd8751b720608e44ff
Jeremy Woertink <jeremywoertink@gmail.com>
2020-09-05 18:27:49 -0700

    Updated app to Lucky 0.24 (#415)
    * Updated app to Lucky 0.24
    
    * rename this to match the other lucky projects
    
    * a few readme updates with another link to the new Discord
    
    * forgot about these files
    
    * We must have missed this update from a previous version. Not sure if this is what's causing the port failure on the CI
    
    * This might be the issue on the CI. Something with containers...
    
    * This is one of the optional updates from 0.24 upgrade
    
    * Adding a small pause before this API spec gives time for the spec server to be running.

--- spec/requests/api/html_conversions/create_spec.cr
+++ spec/requests/api/html_conversions/create_spec.cr
@@ -2,7 +2,10 @@ require "../../../spec_helper"
 
 describe Api::HtmlConversions::Create do
   it "returns a 200 response with converted content" do
-    response = AppClient.exec(Api::HtmlConversions::Create.with(input: "<div>Test</div>"))
+    # NOTE: This is required because this spec runs
+    # before the spec server is fully booted.
+    sleep 0.5
+    response = ApiClient.exec(Api::HtmlConversions::Create.with(input: "<div>Test</div>"))
 
     response.should send_json(200, content: "div \"Test\"\n")
   end
@@ -10,7 +13,7 @@ describe Api::HtmlConversions::Create do
   it "returns a 503 (Service Unavailable) when HTML conversions are disabled" do
     ENV["DISABLE_HTML_CONVERSION_API"] = "Shut it down!"
 
-    response = AppClient.exec(Api::HtmlConversions::Create.with(input: "<div>Test</div>"))
+    response = ApiClient.exec(Api::HtmlConversions::Create.with(input: "<div>Test</div>"))
 
     response.status_code.should eq(503)
   end
--- src/actions/api_action.cr
+++ src/actions/api_action.cr
@@ -1,4 +1,5 @@
 abstract class ApiAction < Lucky::Action
   # Include modules and add methods that are for all API requests
+  disable_cookies
   accepted_formats [:json]
 end
