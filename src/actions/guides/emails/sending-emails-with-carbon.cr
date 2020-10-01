35169a05a5d1093b8af02099f733d2500b5cf1f7
Stephen Dolan <dolan.stephen1@gmail.com>
2020-10-01 12:14:56 -0400

    Add detailed Carbon Adapter documentation (#437)
    * Add detailed Carbon Adapter documentation
    
    This PR aims to close #432.
    
    It breaks out a separate "Adapters" section, explaining each currently-supported adapter in greater detail, as well as specific options for the DevAdapter like `print_email`.
    
    * Updates from PR suggestions

--- src/actions/guides/emails/sending-emails-with-carbon.cr
+++ src/actions/guides/emails/sending-emails-with-carbon.cr
@@ -7,10 +7,48 @@ class Guides::Emails::SendingEmailsWithCarbon < GuideAction
 
   def markdown : String
     <<-MD
-    ## Configuring Carbon
+    ## Configuring Email
 
-    Lucky generates default configuration in `config/email.cr`. In that file you
-    can add SendGrid keys and change adapters.
+    Lucky leverages the [Carbon](https://github.com/luckyframework/carbon) library for writing, sending, and testing emails. Carbon can be configured using the default file generated with a new Lucky application in `config/email.cr`. In that file you can add SendGrid keys and change adapters.
+
+    ## Adapters
+    
+    Carbon supports a growing number of adapters thanks to contributions from the community.
+
+    ### Dev Adapter
+    
+    The `DevAdapter` ships with Carbon by default, and is useful for handling emails in a development or test environment. It can also be leveraged in production to effectively disable emails.
+
+    There are two ways to leverage the `DevAdapter`. The first is by telling the adapter to simply capture all Carbon output without printing or displaying the email content, which is the default:
+
+    ```crystal
+    # config/email.cr
+    BaseEmail.configure do |settings|
+      settings.adapter = Carbon::DevAdapter.new
+    end
+    ```
+
+    If you want to see your email content printed to your development or test server logs, you can use the optional `print_emails` flag:
+
+    ```crystal
+    # config/email.cr
+    BaseEmail.configure do |settings|
+      settings.adapter = Carbon::DevAdapter.new(print_emails: true)
+    end
+    ```
+
+    ### SendGrid Adapter
+
+    The `SendGridAdapter` ships with Carbon by default, and once configured will send all emails through the [SendGrid](https://sendgrid.com) email service.
+    
+    Initializing the `SendGridAdapter` is as simple as initializing the adapter with your SendGrid API key in `config/email.cr`:
+
+    ```crystal
+    # config/email.cr
+    BaseEmail.configure do |settings|
+      settings.adapter = Carbon::SendGridAdapter.new(api_key: ENV["SEND_GRID_KEY"])
+    end
+    ```
 
     ## Sending and testing emails
 
