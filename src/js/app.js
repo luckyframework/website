e3fb052ecd316339e6c547732fc3d283a58adaf7
Nolan Mayersky <59351042+nolyoi@users.noreply.github.com>
2021-02-06 18:44:44 -0600

    Import only the highlight.js core. (#579)
    The entire highlight.js was being imported, so the lines below importing the individual langs were unnecessary. With this change it only imports the core and the languages you have listed below to import. Just a small optimization I noticed.

--- src/js/app.js
+++ src/js/app.js
@@ -9,7 +9,7 @@ import Turbolinks from "turbolinks";
 RailsUjs.start();
 Turbolinks.start();
 
-import hljs from 'highlight.js';
+import hljs from 'highlight.js/lib/core';
 
 import javascript from 'highlight.js/lib/languages/javascript';
 hljs.registerLanguage('javascript', javascript);
