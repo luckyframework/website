7f85e829ab8e9a8aba834cddc3ef3128d250b7d1
matthewmcgarvey <matthewmcgarvey14@gmail.com>
2021-11-21 21:49:22 -0600

    Initial removal of avram dependency

--- config/database.cr
+++
@@ -1,7 +0,0 @@
-AppDatabase.configure do |settings|
-  settings.credentials = Avram::Credentials.void
-end
-
-Avram.configure do |settings|
-  settings.database_to_migrate = AppDatabase
-end
--- src/shards.cr
+++ src/shards.cr
@@ -4,10 +4,8 @@ require "lucky_env"
 # TODO: https://github.com/luckyframework/lucky_env/issues/5
 # LuckyEnv.load(".env")
 
-require "avram"
 require "lucky"
 require "carbon"
-require "authentic"
 require "cmark"
 require "cadmium_transliterator"
 require "lexbor"
