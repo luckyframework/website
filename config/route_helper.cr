b5a2a05a4525427c14c5186593d945e8425b6fd9
matthewmcgarvey <matthewmcgarvey14@gmail.com>
2022-02-16 22:49:46 -0600

    Configure base url correctly in heroku review apps

--- config/route_helper.cr
+++ config/route_helper.cr
@@ -1,8 +1,12 @@
 # This is used when generating URLs for your application
 Lucky::RouteHelper.configure do |settings|
   if LuckyEnv.production?
-    # Example: https://my_app.com
-    settings.base_uri = ENV.fetch("APP_DOMAIN")
+    # Support for heroku review apps
+    if (heroku_app_name = ENV["HEROKU_APP_NAME"]?) && ENV["HEROKU_PR_NUMBER"]?
+      settings.base_url = "https://#{heroku_app_name}.herokuapp.com"
+    else
+      settings.base_uri = ENV.fetch("APP_DOMAIN")
+    end
   else
     # Set domain to the default host/port in development/test
     settings.base_uri = "http://localhost:#{Lucky::ServerSettings.port}"
