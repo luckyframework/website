94bc292d36e65911657ff2ff8a60b467ccfcf81c
matthewmcgarvey <matthewmcgarvey14@gmail.com>
2022-02-16 22:58:03 -0600

    Fix setting name

--- config/route_helper.cr
+++ config/route_helper.cr
@@ -3,7 +3,7 @@ Lucky::RouteHelper.configure do |settings|
   if LuckyEnv.production?
     # Support for heroku review apps
     if (heroku_app_name = ENV["HEROKU_APP_NAME"]?) && ENV["HEROKU_PR_NUMBER"]?
-      settings.base_url = "https://#{heroku_app_name}.herokuapp.com"
+      settings.base_uri = "https://#{heroku_app_name}.herokuapp.com"
     else
       settings.base_uri = ENV.fetch("APP_DOMAIN")
     end
