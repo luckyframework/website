c132984f38475dfcba57910f6d29601bc4048bd9
Jeremy Woertink <jeremywoertink@gmail.com>
2022-04-08 15:21:08 -0700

    fixes sitemap building, and generates a new sitemap

--- tasks/generate_sitemap.cr
+++ tasks/generate_sitemap.cr
@@ -7,28 +7,28 @@ Sitemapper.configure do |c|
 end
 
 class GenerateSitemap < LuckyTask::Task
-  summary "Generate the sitemap.xml for this luckyframework.org"
+  summary "Generate the sitemap.xml for luckyframework.org"
 
   def call
-    sitemaps = Sitemapper.build do
-      add(Home::Index.path, changefreq: "yearly", priority: 0.9)
-      add(HtmlConversions::New.path, changefreq: "yearly", priority: 0.2)
-      add(Blog::Index.path, changefreq: "monthly", priority: 0.6)
-      add(Learn::Index.path, changefreq: "yearly", priority: 0.4)
-      add(Learn::AwesomeLucky::Index.path, changefreq: "monthly", priority: 0.5)
-      add(Learn::Community::Index.path, changefreq: "monthly", priority: 0.5)
-      add(Learn::Ecosystem::Index.path, changefreq: "monthly", priority: 0.5)
+    sitemaps = Sitemapper.build do |builder|
+      builder.add(Home::Index.path, changefreq: "yearly", priority: 0.9)
+      builder.add(HtmlConversions::New.path, changefreq: "yearly", priority: 0.2)
+      builder.add(Blog::Index.path, changefreq: "monthly", priority: 0.6)
+      builder.add(Learn::Index.path, changefreq: "yearly", priority: 0.4)
+      builder.add(Learn::AwesomeLucky::Index.path, changefreq: "monthly", priority: 0.5)
+      builder.add(Learn::Community::Index.path, changefreq: "monthly", priority: 0.5)
+      builder.add(Learn::Ecosystem::Index.path, changefreq: "monthly", priority: 0.5)
 
       GuidesList.guides.each do |guide|
         modification_time = get_modification_time_of_file(guide.guide_file_path)
-        add(guide.path,
+        builder.add(guide.path,
           lastmod: modification_time,
           changefreq: "monthly",
           priority: 0.7)
       end
 
       PostQuery.new.all_posts_newest_first.each do |post|
-        add(Blog::Show.with(post.slug).path,
+        builder.add(Blog::Show.with(post.slug).path,
           lastmod: post.published_at,
           changefreq: "yearly",
           priority: 0.7)
