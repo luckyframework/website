038f9ddcf3310f1ee0602b5595c751fc832c3aa5
Alex Piechowski <alex@piechowski.io>
2022-04-08 17:23:07 -0500

    Switch from turbolinks to turbo (#949)
    Reverts the previous revert of turbolinks -> turbo
    Fixes html conversions page, which resulted in a turbo error previously

--- src/pages/html_conversions/new_page.cr
+++ src/pages/html_conversions/new_page.cr
@@ -7,26 +7,27 @@ class HtmlConversions::NewPage < PageLayout
       h2 title, class: "text-3xl mb-4"
 
       para "Lucky uses Crystal classes and methods to generate HTML. It may sound crazy at first, but the advantages are numerous. Never accidentally print nil to the page, extract and share partials using regular methods. Easily read an entire page by looking at just the render method. Text is automatically escaped for security. And it’s all type safe. That means no more unmatched closing tags, and never rendering a page with missing data."
-
-      form_for HtmlConversions::Create, class: "my-4 space-y-2" do
-        div class: "space-y-1" do
-          h3 "HTML Input", class: "text-2xl"
-          textarea input, name: "input", class: "appearance-none mt-2 border-2 border-gray-200 rounded w-full py-2 px-4 leading-tight focus:outline-none focus:bg-white", placeholder: "Paste your HTML here", attrs: [:required], rows: 4
-        end
-
-        render_down_arrow
-        render_convert_button
-        render_down_arrow
-
-        div class: "space-y-1" do
-          div class: "flex items-center justify-between" do
-            h3 "Lucky Output", class: "text-2xl"
-            render_copy_button
+      tag "turbo-frame", id: "html-conversion" do
+        form_for HtmlConversions::Create, class: "my-4 space-y-2" do
+          div class: "space-y-1" do
+            h3 "HTML Input", class: "text-2xl"
+            textarea input, name: "input", class: "appearance-none mt-2 border-2 border-gray-200 rounded w-full py-2 px-4 leading-tight focus:outline-none focus:bg-white", placeholder: "Paste your HTML here", attrs: [:required], rows: 4
           end
 
-          pre class: "mt-2 #{output_default_height}" do
-            code class: "h-full language-crystal hljs" do
-              text output
+          render_down_arrow
+          render_convert_button
+          render_down_arrow
+
+          div class: "space-y-1" do
+            div class: "flex items-center justify-between" do
+              h3 "Lucky Output", class: "text-2xl"
+              render_copy_button
+            end
+
+            pre class: "mt-2 #{output_default_height}" do
+              code class: "h-full language-crystal hljs" do
+                text output
+              end
             end
           end
         end
