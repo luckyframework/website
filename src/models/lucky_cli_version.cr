2a6d2747c398037ff3cf01e5636d4624b94c0cf6
Jeremy Woertink <jeremywoertink@gmail.com>
2022-04-08 14:59:25 -0700

    Upgrade the site to use Lucky 0.30.0 (#973)

--- src/models/lucky_cli_version.cr
+++ src/models/lucky_cli_version.cr
@@ -8,7 +8,7 @@ module LuckyCliVersion
   end
 
   def current_version : SemanticVersion
-    SemanticVersion.new(0, 29, 0)
+    SemanticVersion.new(0, 30, 0)
   end
 
   def min_compatible_crystal_version : SemanticVersion
@@ -16,6 +16,6 @@ module LuckyCliVersion
   end
 
   def max_compatible_crystal_version : SemanticVersion
-    SemanticVersion.new(1, 2, 2)
+    SemanticVersion.new(1, 4, 0)
   end
 end
