a06de2587608f894e8b5d1f3f4fdb3dfe86bf562
Michael Wagner <mdwranger@gmail.com>
2023-06-17 10:42:06 -0400

    fix: formatting

--- src/pages/team/index_page.cr
+++ src/pages/team/index_page.cr
@@ -75,9 +75,9 @@ class Team::IndexPage < PageLayout
   end
 
   private def render_member(member : Member)
-    tag "object", data: member.avatar, class: "mx-auto h-56 w-56 rounded-full", type: "image/jpeg", title: member.fullname  do
+    tag "object", data: member.avatar, class: "mx-auto h-56 w-56 rounded-full", type: "image/jpeg", title: member.fullname do
       tag "svg", aria_hidden: "true", fill: "currentColor", viewbox: "0 0 512 512", xmlns: "http://www.w3.org/2000/svg" do
-          tag "path", d: "M399 384.2C376.9 345.8 335.4 320 288 320H224c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z"
+        tag "path", d: "M399 384.2C376.9 345.8 335.4 320 288 320H224c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z"
       end
     end
     h3 member.fullname, class: "mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900"
