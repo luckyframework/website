f9205557ad39dc75ec4e6075369b2ad6fb000ecd
Jeremy Woertink <jeremywoertink@gmail.com>
2024-10-23 07:58:47 -0700

    applying suggestions

--- src/posts/lucky_1.3.0.cr
+++ src/posts/lucky_1.3.0.cr
@@ -8,8 +8,8 @@ class Lucky130Release < BasePost
 
   def summary : String
     <<-TEXT
-    Closing out the final quarter of 2024 with Crystal 1.14.0 out and full
-    compatibility with the new Lucky 1.3.0 release.
+    Closing out the final quarter of 2024 with improved attack protection, better Windows compatibility,
+    arbitrary CLI tasks, and more.
     TEXT
   end
 
@@ -29,13 +29,13 @@ class Lucky130Release < BasePost
     ## Here's what's new
 
     ### New MaximumRequestSizeHandler
-    
+
     This is a new handler you can add to your server middleware stack allowing you to customize
     the maximum size your requests should allow. This may help you to prevent your app from being
     attacked with extra large file sizes or payloads.
 
-    Add `Lucky::MaximumRequestSizeHandler.new` to your `src/app_server.cr` 
-    
+    Add `Lucky::MaximumRequestSizeHandler.new` to your `src/app_server.cr`
+
     ```crystal
     def middleware : Array(HTTP::Handler)
       [
@@ -55,29 +55,43 @@ class Lucky130Release < BasePost
       settings.max_size = 10_485_760 # 10MB
     end
     ```
-    
+
     ### More support for Windows
-    
+
     Our previous release 1.2.0 added the ability to install the Lucky CLI on Windows using [Scoop](https://scoop.sh/),
     but booting the Lucky app still had quite a few issues.
 
     This release has put a lot more focus in to Windows compatibility with most of the eco system shards fully working.
 
-    The following shards all now have Windows compatibility: [Lucky](https://github.com/luckyframework/lucky), [LuckyFlow](https://github.com/luckyframework/lucky_flow), [Carbon](https://github.com/luckyframework/carbon), [Pulsar](https://github.com/luckyframework/pulsar), [LuckyCache](https://github.com/luckyframework/lucky_cache), [LuckyRouter](https://github.com/luckyframework/lucky_router), [Habitat](https://github.com/luckyframework/habitat), [LuckyEnv](https://github.com/luckyframework/lucky_env), [LuckyTask](https://github.com/luckyframework/lucky_task), [Wordsmith](https://github.com/luckyframework/wordsmith), [LuckyTemplate](https://github.com/luckyframework/lucky_template), and [LuckySecTester](https://github.com/luckyframework/lucky_sec_tester).
+    The following shards all now have Windows compatibility:
+
+    * [Lucky](https://github.com/luckyframework/lucky)
+    * [LuckyFlow](https://github.com/luckyframework/lucky_flow)
+    * [Carbon](https://github.com/luckyframework/carbon)
+    * [Pulsar](https://github.com/luckyframework/pulsar)
+    * [LuckyCache](https://github.com/luckyframework/lucky_cache)
+    * [LuckyRouter](https://github.com/luckyframework/lucky_router)
+    * [Habitat](https://github.com/luckyframework/habitat)
+    * [LuckyEnv](https://github.com/luckyframework/lucky_env)
+    * [LuckyTask](https://github.com/luckyframework/lucky_task)
+    * [Wordsmith](https://github.com/luckyframework/wordsmith)
+    * [LuckyTemplate](https://github.com/luckyframework/lucky_template)
+    * [LuckySecTester](https://github.com/luckyframework/lucky_sec_tester)
+
     The final shard missing is [Avram](https://github.com/luckyframework/avram) (which is mainly held back by [this issue on PG](https://github.com/will/crystal-pg/issues/291)), and
     the [CLI itself](https://github.com/luckyframework/lucky_cli) which will be finalized once Avram is updated.
 
     If you're a developer using Windows, we would love the additional assistance. Reach out on [Discord](#{Chat::Index.path})
     if you have questions.
-    
+
     ### Arbitrary CLI tasks
-    
+
     One major issue that held Lucky back on Windows compatibility was the use of the `postinstall` scripts on Lucky, Carbon, and Avram.
     Previously, when installing these shards, they would need to run a bash script that would then precompile a few binaries
     for use with your app. For example, the `lucky gen.secret_key` task which generates a random unique key.
 
     Aside from these bash scripts not being directly usable on Windows, there's also been discussion that [postinstall can be "harmful"](https://forum.crystal-lang.org/t/shards-postinstall-considered-harmful/3910)
- and may possibly removed in a future version of Crystal. By removing the use of the postinstall,
+    and may possibly removed in a future version of Crystal. By removing the use of the postinstall,
     Lucky was able to do a few things:
 
     * Installing the shards is **way** faster since it no longer needs to stop and precompile several binaries
@@ -94,7 +108,7 @@ class Lucky130Release < BasePost
     Next time you execute this call, it'll run the compiled version instantly.
 
     > The next release, a new `lucky build` command will make this easier.
-    
+
     ## Parting words
 
     Thank you for sticking with Lucky. While there's many options out there to choose from,
